a76e995976b793557feb54be0db97b792bf6643b
Stella Liang <stellaliang@Stellas-MacBook-Pro.local>
2019-04-02 14:15:48 -0700

    Render name, date, image, and review body

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,18 +1,48 @@
 import React from 'react';
+import $ from 'jquery';
+import ListingList from './ListingList.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      expanded: false,
+      mockData: {
+        reviews: [],
+      }
     };
+    this.getMockData = this.getMockData.bind(this);
   }
+
+  componentDidMount() {
+    this.getMockData();
+  }
+
+  getMockData() {
+    $.ajax({
+      url: '/reviews/1',
+      type: 'GET',
+      success: (data) => {
+        console.log('data!', data);
+        this.setState({
+          mockData: data,
+        });
+      },
+      error: () => {console.log('error')}
+    });
+  }
+    
+
   render() {
     return (
       <div>
-        WEBPACK SETUP
+        <div>
+          Hello
+        </div>
+        <div>{`${this.state.mockData.reviews.length} Reviews`}</div>
+        <div><ListingList listing={this.state.mockData.reviews} /></div>
       </div>
-    )
+    );
   }
 }
 
