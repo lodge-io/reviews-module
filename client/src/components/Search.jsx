fe42615d5cf104bf22d25457fbe9bb9ecaace790
Stella Liang <stellaliang@stellas-mbp.attlocal.net>
2019-04-04 21:34:28 -0700

    Implement a button for searching reviews

---
+++ client/src/components/Search.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Search = props => (
+  <div>
+    <input 
+      type="text"
+      onChange={(event) => {props.handleChange(event)}}/>
+    <button className="searchButton" onClick={props.handleClick}>Go</button>
+  </div>
+)
+
+
+export default Search
