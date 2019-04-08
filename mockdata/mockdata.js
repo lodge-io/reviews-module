5b2da48246021ffc6b85b39eb3c13e3e7d741f54
Stella Liang <stellaliang@Stellas-MacBook-Pro.local>
2019-04-07 18:43:48 -0700

    Change the minimum number for star rating

--- mockdata/mockdata.js
+++ mockdata/mockdata.js
@@ -1,33 +1,33 @@
 const faker = require('faker');
 
-const createUserReview = () => {
-  const reviews = [];
-  for (let x = 1; x <= 100; x += 1) {
+const createUserReviews = () => {
+  const listings = [];
+  for (let x = 1; x <= 5; x += 1) {
     const userId = {
       id: x,
     };
-    const array = [];
-    reviews.push(userId);
-    const randomNumber = Math.floor(Math.random() * Math.floor(15));
+    const reviews = [];
+    listings.push(userId);
+    const randomNumber = Math.random() * (20 - 9) + 9;
     for (let i = 0; i < randomNumber; i += 1) {
       const user = {
         name: faker.name.firstName(),
         date: `${faker.date.month()} ${faker.random.number({ min: 2000, max: 2019 })}`,
         image: faker.image.avatar(),
-        reviewbody: faker.lorem.paragraph(),
-        accuracy: faker.random.number(5),
-        communication: faker.random.number(5),
-        cleanliness: faker.random.number(5),
-        location: faker.random.number(5),
-        checkin: faker.random.number(5),
-        value: faker.random.number(5),
+        reviewbody: faker.lorem.paragraph(15),
+        accuracy: faker.random.number({ min: 2, max: 5 }),
+        communication: faker.random.number({ min: 2, max: 5 }),
+        cleanliness: faker.random.number({ min: 2, max: 5 }),
+        location: faker.random.number({ min: 2, max: 5 }),
+        checkin: faker.random.number({ min: 2, max: 5 }),
+        value: faker.random.number({ min: 2, max: 5 }),
       };
-      array.push(user);
+      reviews.push(user);
     }
-    userId.reviews = array;
+    userId.reviews = reviews;
   }
-  return reviews;
+  return listings;
 };
 
 
-module.exports.createUserReview = createUserReview;
+module.exports.createUserReviews = createUserReviews;
